3c1271632d6b7b5bcc51de03083a594c819bf08d
Nithishkumar654 <nithishkumar.s654@gmail.com>
2022-12-31 18:46:13 +0530

    4th

--- src/components/Navigationbar.js
+++ src/components/Navigationbar.js
@@ -15,7 +15,7 @@ function Navigationbar() {
   return (
     <div className='bg-light mt-1 fw-bold'>
       <nav className="navbar navbar-expand-lg navbar-light pb-0">
-  <div className="collapse navbar-collapse" id="navbarNav">
+  <div  className="collapse navbar-collapse" id="navbarNav">
     <ul className="navbar-nav mt-2 fs-5">
       <li className="nav-item">
         <NavLink className="nav-link" style={({isActive})=>{return isActive?activeLink:inactiveLink}} to="/">AddTask</NavLink>
